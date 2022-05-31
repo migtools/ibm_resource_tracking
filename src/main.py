13e49b400258215f53bd001fe604c895268b769c
Ankur Mundra <amundra@redhat.com>
2022-05-31 14:00:27 -0400

    Extending on usage_report

--- src/main.py
+++ src/main.py
@@ -1,22 +1,27 @@
-import configparser
+import json
 
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_vpc import VpcV1
+from ibm_cloud_sdk_core import ApiException
 
-from vpc_infrastructure import get_all_vpcs, get_all_subnets, get_all_instances, get_all_public_gateways
+from src.config_helper import get_user_management_service, get_account_id
+from usage_report import get_account_summary, get_resource_usage
+from vpc_infrastructure import get_vpc_infrastructure_data, print_vpc_infrastructure_data
 
-config = configparser.ConfigParser()
-config.read('../config/ibm_config.ini')
 
-# Get IAM API key from config file
-iam_apikey = config['IAM']['apikey']
-# Create an IAM authenticator.
-authenticator = IAMAuthenticator(iam_apikey)
+# List users
+def get_all_users():
+    print("Listing Users")
+    try:
+        service = get_user_management_service()
+        users = service.list_users(account_id=get_account_id()).get_result()['resources']
+        print(json.dumps(users, indent=2))
+    except ApiException as e:
+        print("List Users failed with status code: {0} : {1}".format(e.code, e.message))
+
 
 if __name__ == '__main__':
-    service = VpcV1(authenticator=authenticator)
-    service.set_service_url(config['VPC']['endpoint_url'])
-    get_all_vpcs(service)
-    get_all_subnets(service)
-    get_all_instances(service)
-    get_all_public_gateways(service)
+    data = get_vpc_infrastructure_data()
+    print_vpc_infrastructure_data(data)
+
+    get_account_summary()
+    get_resource_usage()
+    # get_all_users()
--- src/vpc_infrastructure.py
+++ src/vpc_infrastructure.py
@@ -1,46 +1,69 @@
 from ibm_cloud_sdk_core import ApiException
+from config_helper import get_vpc_service
+
+vpc_infrastructure_data = {'VPCs': [], 'Subnets': [], 'Instances': [], 'Public Gateways': []}
 
 
-# List all VPCs
-def get_all_vpcs(service):
-    print("List all VPCs")
+def get_all_vpcs():
     try:
+        service = get_vpc_service()
         vpcs = service.list_vpcs().get_result()['vpcs']
-        print_resource_details(vpcs)
+        add_resource_details(vpcs, 'VPCs')
     except ApiException as e:
         print("List VPCs failed with status code: {0} : {1}".format(e.code, e.message))
 
 
 # Listing Subnets
-def get_all_subnets(service):
-    print("Listing Subnets")
+def get_all_subnets():
     try:
+        service = get_vpc_service()
         subnets = service.list_subnets().get_result()['subnets']
-        print_resource_details(subnets)
+        add_resource_details(subnets, 'Subnets')
     except ApiException as e:
         print("List Subnets failed with status code: {0} : {1}".format(e.code, e.message))
 
 
 # Listing Public Gateways
-def get_all_public_gateways(service):
-    print("Listing Public Gateways")
+def get_all_public_gateways():
     try:
+        service = get_vpc_service()
         gateways = service.list_public_gateways().get_result()['public_gateways']
-        print_resource_details(gateways)
+        add_resource_details(gateways, 'Public Gateways')
     except ApiException as e:
         print("List Public Gateways failed with status code: {0} : {1}".format(e.code, e.message))
 
 
 # Listing Instances
-def get_all_instances(service):
-    print("Listing Instances")
+def get_all_instances():
     try:
+        service = get_vpc_service()
         instances = service.list_instances().get_result()['instances']
-        print_resource_details(instances)
+        add_resource_details(instances, 'Instances')
     except ApiException as e:
         print("List Instances failed with status code: {0} : {1}".format(e.code, e.message))
 
 
-def print_resource_details(resources):
+def add_resource_details(resources, resource_type):
     for resource in resources:
-        print(resource['id'], "\t", resource['name'], "\t", resource['created_at'], "\t", resource['status'])
+        # Create a dictionary of resource details
+        resource_details = {'id': resource['id'], 'name': resource['name'],
+                            'status': resource['status'], 'created_at': resource['created_at']}
+        # Add the resource details to the global dictionary
+        vpc_infrastructure_data[resource_type].append(resource_details)
+
+
+def print_vpc_infrastructure_data(data):
+    for resource_type, resources in data.items():
+        print('Listing', resource_type)
+        for resource in resources:
+            print(resource)
+        print('\n')
+
+
+def get_vpc_infrastructure_data():
+    get_all_vpcs()
+    get_all_subnets()
+    get_all_instances()
+    get_all_public_gateways()
+    print('Data retrieved successfully')
+    return vpc_infrastructure_data
